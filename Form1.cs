af6b2d05e801d6a0ea07cf852fc1035d0ca439a7
Goko600 <110344708+Goko600@users.noreply.github.com>
2022-08-01 16:44:34 +0300

    Entry-Screen

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace ilk_proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = textBox2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = textBox3.Text;
+        }
+    }
+}
